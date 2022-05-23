afddcb1d2bfc952ea9a66f733e132545cb7e98cb
PengMvc <xiaopeng.zhang01@sap.com>
2022-05-23 12:45:51 +0800

    init springboot project

---
+++ src/main/java/SpringBootApplication.java
@@ -0,0 +1,12 @@
+import com.apple.eawt.Application;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * SpringBoot启动类
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class,args);
+    }
+}
