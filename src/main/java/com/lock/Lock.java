9d1f82ffa55e232d8b097f9c4f2b0cbdb5e44eba
PengMvc <2301129239@qq.com>
2022-05-27 22:32:39 +0800

    add lock

---
+++ src/main/java/com/lock/Lock.java
@@ -0,0 +1,12 @@
+package com.lock;
+
+/**
+ * locks of project
+ * @Author: PengMvc
+ * @Date: 2022/05/27/22:30
+ */
+public class Lock {
+
+    /** lock of place order*/
+    private static final String PLACE_ORDER="placeOrder";
+}
