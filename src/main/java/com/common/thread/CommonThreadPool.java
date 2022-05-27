f51faaaf611d821a0fdc8761f1683c8008e60acf
PengMvc <2301129239@qq.com>
2022-05-27 14:32:17 +0800

    add threadpool

---
+++ src/main/java/com/common/thread/CommonThreadPool.java
@@ -0,0 +1,34 @@
+package com.common.thread;
+
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 手动定义线程池参数，避免出现OOM
+ * 采用的是无界队列（当任务数大于核心线程数，多余的任务在队列排队
+ * 到有空闲的线程才去处理）
+ * @Author: PengMvc
+ * @Date: 2022/05/26/20:37
+ */
+public class CommonThreadPool {
+
+    // 核心线程池
+    private static final int CORE_POOL_SIZE = 10;
+    // 线程活跃时间
+    private static final int KEEP_ALIVE_TIME = 60;
+
+    private static final ThreadPoolExecutor EXECUTOR = new ThreadPoolExecutor(
+            CORE_POOL_SIZE,
+            Integer.MAX_VALUE,
+            KEEP_ALIVE_TIME, TimeUnit.SECONDS,
+            new LinkedBlockingDeque<>(),
+            new DefaultThreadFactory("CommonThreadPool",false),
+            new ThreadPoolExecutor.AbortPolicy());
+
+    public static ThreadPoolExecutor getExecutor() {
+        return EXECUTOR;
+    }
+}
---
+++ src/main/java/com/common/thread/SpringContextPool.java
@@ -0,0 +1,52 @@
+package com.common.thread;
+
+import io.netty.util.concurrent.DefaultThreadFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ *  继承父线程http请求上下文
+ * @Author: PengMvc
+ * @Date: 2022/05/26/20:42
+ */
+public class SpringContextPool {
+
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAX_POOL_SIZE = 20;
+    private static final int QUEUE_CAPACITY = 1024;
+    private static final int KEEP_ALIVE_TIME = 60;
+
+    private static final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+    static {
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setKeepAliveSeconds(KEEP_ALIVE_TIME);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadFactory(new DefaultThreadFactory("context-pool", false));
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+        executor.setTaskDecorator(runnable -> {
+            try {
+                RequestAttributes attributes = RequestContextHolder.currentRequestAttributes();
+                return () -> {
+                    try {
+                        RequestContextHolder.setRequestAttributes(attributes);
+                        runnable.run();
+                    } finally {
+                        RequestContextHolder.resetRequestAttributes();
+                    }
+                };
+            } catch (IllegalStateException e) {
+                return runnable;
+            }
+        });
+        executor.initialize();
+    }
+
+    public static ThreadPoolTaskExecutor getExecutor() {
+        return executor;
+    }
+}
